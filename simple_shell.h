98913321a65d7a15b4f5838f3c04718b758dcd91
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-07-13 11:24:06 +0000

    header file

---
+++ simple_shell.h
@@ -0,0 +1,16 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+
+void prompt_user(char **av, char **envt);
+void prompt_user2(char **av, char **envt);
+int main(int ac, char **av, char **envt);
+char *handl_getline();
+
+#endif
