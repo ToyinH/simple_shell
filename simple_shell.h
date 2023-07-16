6703c6db7b24ca8ae80c21ca24b5ad9d7d2fbdf7
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-07-16 21:33:56 +0000

    updated header file

--- simple_shell.h
+++ simple_shell.h
@@ -7,10 +7,16 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdbool.h>
+#include <dirent.h>
 
 void prompt_user(char **av, char **envt);
 int main(int ac, char **av, char **envt);
 char *handl_getline();
 char **handl_strtok(char *string, char *delimiter);
+int print_env(void);
+int custom_strcmp(const char *s1, const char *s2);
+int path_check(char *string);
+char *file_check(char *string);
+void fork_exec(char **argv, char **env, char **av);
 
 #endif
