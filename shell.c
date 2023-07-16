6d035a688aa08104c9fa83cdb6e52227667e3f7d
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-07-16 21:33:35 +0000

    updated shell.c file which contains the main function

--- shell.c
+++ shell.c
@@ -4,15 +4,15 @@
  * main - function that creates my shell
  * @ac: argument count;
  * @av: argument vector
- * @envt: environment
+ * @env: environment
  *
  * Return: return 0 if successful
  */
-int main(int ac, char **av, char **envt)
+int main(int ac, char **av, char **env)
 {
 	if (ac == 1)
 	{
-		prompt_user(av, envt);
+		prompt_user(av, env);
 	}
 	return (0);
 }
