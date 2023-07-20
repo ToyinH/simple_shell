454193e477f7ce0b31e9b05e68c201fd1ac10caa
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-07-20 17:07:39 +0000

    updated header file

--- simple_shell.h
+++ simple_shell.h
@@ -18,5 +18,7 @@ int custom_strcmp(const char *s1, const char *s2);
 int path_check(char *string);
 char *file_check(char *string);
 void fork_exec(char **argv, char **env, char **av);
+ssize_t custom_read(int fd, void *buf, size_t count);
+ssize_t custom_getline(char **lineptr, size_t *n);
 
 #endif
