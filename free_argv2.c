005fa5d4283dc2aca417fb76a963a0bd75e736d2
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-07-22 09:50:23 +0000

    undated function to free memory with skipping previously freed

--- free_argv2.c
+++ free_argv2.c
@@ -5,15 +5,9 @@
  */
 void free_argv2(char **argv)
 {
-	int i = 1;
-
 	if (argv == NULL)
 		return;
-	while (argv[i] != NULL)
-	{
-		free(argv[i - 1]);
-		i++;
-	}
+	free(argv[0]);
 	free(argv);
 }
 
