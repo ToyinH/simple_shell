f82bb180da78b97851772e200add6988062601cd
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-07-22 08:11:37 +0000

    function to free array of strings skipping the last string as it has been freed before

--- free_argv2.c
+++ free_argv2.c
@@ -1,6 +1,6 @@
 #include "simple_shell.h"
 /**
- * free_argv - function to free argv skipping the last string in array
+ * free_argv2 - function to free argv skipping the last string in array
  * @argv: arg vector
  */
 void free_argv2(char **argv)
