3047269f7ed82ed7482cc5234009043a325cfb7c
Gbeminiyi Oke <adunolafoyinsola@gmail.com>
2023-07-23 14:08:30 +0000

    updated strcat

--- custom_strcat.c
+++ custom_strcat.c
@@ -1,27 +1,22 @@
 #include "simple_shell.h"
 
 /**
- * strcat - this function concatenates two strings
+ * _strcat - this function concatenates two strings
  * @str1: first string
  * @str2: second string
- * @n_byte: bytes to be used
  * Return: the result of the concatenation.
  */
 
-char *_strcat(char *str1, char *str2, int n_byte)
+char *_strcat(char *str1, char *str2)
 {
-	char *new_str = str1;
 	int i = 0, j = 0;
 
 	while (str1[i] != '\0')
 		i++;
-	while (str2[j] != '\0' && j < n_byte)
+	for (j = 0; str2[j] != '\0'; j++, i++)
 	{
 		str1[i] = str2[j];
-		i++;
-		j++;
 	}
-	if (j < n_byte)
-		str1[i] = '\0';
-	return (new_str);
+	str1[i] = '\0';
+	return (str1);
 }
