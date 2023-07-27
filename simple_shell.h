f21ed18eb033537855d2d66b65abeb5a2ced1426
Gbeminiyi Oke <adunolafoyinsola@gmail.com>
2023-07-27 01:57:40 +0000

    update

--- simple_shell.h
+++ simple_shell.h
@@ -1,13 +1,21 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 #include <stdio.h>
+
 #include <string.h>
+
 #include <unistd.h>
+
 #include <stdlib.h>
+
 #include <sys/types.h>
+
 #include <sys/wait.h>
+
 #include <stdbool.h>
+
 #include <dirent.h>
+
 #include <signal.h>
 
 void prompt_user(char **av, char **envt);
