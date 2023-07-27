cfe4b9ae8cda3e6bf6d53d8e739852ed873edabb
Gbeminiyi Oke <adunolafoyinsola@gmail.com>
2023-07-27 01:32:22 +0000

    update

---
+++ fork_exec.c
@@ -0,0 +1,29 @@
+#include "simple_shell.h"
+/**
+ * fork_exec - function that fork and execute function
+ * @argv: argument vector to track tokens
+ * @env: environment
+ * @av: argument vector
+ */
+void fork_exec(char **argv, char **env, char **av)
+{
+	pid_t child_pid;
+	int status = 0;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		free_argv(argv);
+		exit(EXIT_FAILURE);
+	}
+	if (child_pid == 0)
+	{
+		if (execve(argv[0], argv, env) == -1)
+		{
+			_printf(av[0]);
+			_printf(": No such file or directory\n");
+		}
+	}
+	else
+		wait(&status);
+}
