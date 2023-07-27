69782a64a6efba4238a031a92b63ee00ae0f5462
Gbeminiyi Oke <adunolafoyinsola@gmail.com>
2023-07-27 01:53:37 +0000

    update

--- simple_shell.h
+++ simple_shell.h
@@ -11,13 +11,21 @@
 #include <signal.h>
 
 void prompt_user(char **av, char **envt);
+
 int main(int ac, char **av, char **envt);
+
 char *handl_getline();
+
 char **handl_strtok(char *string, char *delimiter);
+
 int print_env(void);
+
 int custom_strcmp(const char *s1, const char *s2);
+
 int path_check(char *string);
+
 char *file_check(char *string);
+
 void fork_exec(char **argv, char **env, char **av);
 ssize_t custom_read(int fd, void *buf, size_t count);
 ssize_t custom_getline(char **lineptr, size_t *n);
@@ -26,13 +34,21 @@ void free_argv2(char **argv);
 void premade_path(char **argv, char **av, char **env, char *str);
 void unpath(char **argv, char **av, char **env, char *str);
 
+
 char *_strcat(char *str1, char *str2);
+
 char *_strcpy(char *str_to, char *str1);
+
 int _strlen(char *str);
+
 void _printf(char *str_in);
+
 char *_realloc(char *pntr, size_t new_mem_size);
+
 char *_memcpy(char *dest, char *src, size_t n);
+
 void handle_sigint (int signum __attribute__((unused)));
+
 typedef void (*sighandler_t)(int);
 
 #endif
