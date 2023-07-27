fb81f1d4cb4b62486e608c2a6652d3bed929a78d
Gbeminiyi Oke <adunolafoyinsola@gmail.com>
2023-07-27 01:51:36 +0000

    update

--- simple_shell.h
+++ simple_shell.h
@@ -21,17 +21,11 @@ char **handl_strtok(char *string, char *delimiter);
 int print_env(void);
 
 int custom_strcmp(const char *s1, const char *s2);
-
 int path_check(char *string);
-
 char *file_check(char *string);
-
 void fork_exec(char **argv, char **env, char **av);
-
 ssize_t custom_read(int fd, void *buf, size_t count);
-
 ssize_t custom_getline(char **lineptr, size_t *n);
-
 void free_argv(char **argv);
 void free_argv2(char **argv);
 void premade_path(char **argv, char **av, char **env, char *str);
