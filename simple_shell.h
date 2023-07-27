6996d6413b82e14d55d9a7db712725a2feae47bd
Gbeminiyi Oke <adunolafoyinsola@gmail.com>
2023-07-27 01:56:59 +0000

    update

--- simple_shell.h
+++ simple_shell.h
@@ -11,21 +11,13 @@
 #include <signal.h>
 
 void prompt_user(char **av, char **envt);
-
 int main(int ac, char **av, char **envt);
-
 char *handl_getline();
-
 char **handl_strtok(char *string, char *delimiter);
-
 int print_env(void);
-
 int custom_strcmp(const char *s1, const char *s2);
-
 int path_check(char *string);
-
 char *file_check(char *string);
-
 void fork_exec(char **argv, char **env, char **av);
 ssize_t custom_read(int fd, void *buf, size_t count);
 ssize_t custom_getline(char **lineptr, size_t *n);
