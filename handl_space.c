a41de238ebf701249661bf26fd54af12e85ff6a0
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-07-28 13:01:32 +0000

    function to handle space characters

---
+++ handl_space.c
@@ -0,0 +1,46 @@
+#include "simple_shell.h"
+/**
+ * space_character - function to check if no space character or enter key
+ * @strn: string
+ *
+ * Return: true if it contains character and false if otherwise
+ */
+
+bool space_character(char *strn)
+{
+	int i;
+
+	if (strn[0] == '\0')
+	{
+		return (false);
+	}
+	if (strn[0] == '\n')
+	{
+		return (false);
+	}
+	for (i = 0; i < len(strn); i++)
+	{
+		if (strn[i] != ' ')
+		{
+			return (true);
+		}
+	}
+	return (false);
+}
+
+/**
+ * len - length of string ending with newline
+ * @s: string
+ *
+ * Return: return string length
+ */
+int len(char *s)
+{
+	int j = 0;
+
+	while (s[j] != '\n')
+	{
+		j++;
+	}
+	return (j);
+}
