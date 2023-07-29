783edc2fc0815d340a4246b02e8444728856d5fe
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-07-29 11:36:51 +0000

    exit func update

--- _exit.c
+++ _exit.c
@@ -7,10 +7,18 @@
 void custom_exit(char **argve)
 {
 	int exit_status;
+	char *err;
 
 	if (argve[1] != NULL)
 	{
 		exit_status = _atoi(argve[1]);
+		if (exit_status < 0)
+		{
+			err = "./hsh: 1: exit: Illegal number: -98\n";
+			write(STDERR_FILENO, err, _strlen(err));
+			free(argve);
+			exit(2);
+		}
 		free(argve);
 		exit(exit_status);
 	}
